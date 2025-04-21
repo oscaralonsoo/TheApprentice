26afa011f8e2f5ead66f131cf5d2dcdac5e78b87
TuMamaMeGusta <javigg011205@gmail.com>
2025-04-22 00:27:36 +0200

    vidas fixed

--- Output/src/PlayerMechanics.cpp
+++ Output/src/PlayerMechanics.cpp
@@ -546,7 +546,7 @@ void PlayerMechanics::HandleLifes()
     if (vidas <= 0) 
     {
         printf("ENTRAAAAAAA");
-        Engine::GetInstance().scene.get()->ReloadCurrentSceneAtCheckpoint();
+        player->SetPosition(Vector2D(1950, 650));
         vidas = 3;
     }
 }

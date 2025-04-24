de408a73dd9e113b0c6e2da41e19a16dbac7950f
Pascra <arnaupascual40@gmail.com>
2025-04-24 18:55:36 +0200

    Laberinto cambiado

--- Output/src/Player.cpp
+++ Output/src/Player.cpp
@@ -21,7 +21,7 @@ Player::Player() : Entity(EntityType::PLAYER)
 Player::~Player() {}
 
 bool Player::Awake() {
-	position = Vector2D(497, 888);
+	position = Vector2D(17760, 6968);
 	return true;
 }
 

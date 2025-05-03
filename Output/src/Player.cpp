708eb5b44888daf3f7b232b4eb7dde004c12acc1
Pascra <160216424+Pascra@users.noreply.github.com>
2025-05-03 17:53:28 +0200

    Recortes manglar 1.02

--- Output/src/Player.cpp
+++ Output/src/Player.cpp
@@ -21,7 +21,7 @@ Player::Player() : Entity(EntityType::PLAYER)
 Player::~Player() {}
 
 bool Player::Awake() {
-	position = Vector2D(5164, 10373);
+	position = Vector2D(7048, 4880);
 	return true;
 }
 

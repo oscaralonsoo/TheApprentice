af4954bd9ff8103343d58671796f27972fdbaf8b
Pascra <160216424+Pascra@users.noreply.github.com>
2025-05-07 18:10:22 +0200

    Escenario 4 definitivo

--- Output/src/Player.cpp
+++ Output/src/Player.cpp
@@ -21,7 +21,7 @@ Player::Player() : Entity(EntityType::PLAYER)
 Player::~Player() {}
 
 bool Player::Awake() {
-	position = Vector2D(7048, 4880);
+	position = Vector2D(10194, 7137);
 	return true;
 }
 

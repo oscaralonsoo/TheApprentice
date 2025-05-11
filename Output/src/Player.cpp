716d0ec1d671e15878a97cc281e885b48616dc33
oscaralonsoo <alonsocamenforte@gmail.com>
2025-05-11 03:18:36 +0200

    map41 upper left

--- Output/src/Player.cpp
+++ Output/src/Player.cpp
@@ -21,7 +21,7 @@ Player::Player() : Entity(EntityType::PLAYER)
 Player::~Player() {}
 
 bool Player::Awake() {
-	position = Vector2D(5952, 5632);
+	position = Vector2D(9480, 5322);
 	return true;
 }
 

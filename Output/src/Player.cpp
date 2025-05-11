3f9f8fd892397882c3739e6eb0a4c7823f53b57f
oscaralonsoo <alonsocamenforte@gmail.com>
2025-05-12 00:29:08 +0200

    delete jpg

--- Output/src/Player.cpp
+++ Output/src/Player.cpp
@@ -21,7 +21,7 @@ Player::Player() : Entity(EntityType::PLAYER)
 Player::~Player() {}
 
 bool Player::Awake() {
-	position = Vector2D(13760, 4608);
+	position = Vector2D(6016, 5632);
 	return true;
 }
 

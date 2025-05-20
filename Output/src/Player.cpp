549f69ccc42797a03de211c8058eff7c66588638
Deivid13SB <158307930+Deivid13SB@users.noreply.github.com>
2025-05-20 23:29:30 +0200

    Music Menu & Cave

--- Output/src/Player.cpp
+++ Output/src/Player.cpp
@@ -21,7 +21,7 @@ Player::Player() : Entity(EntityType::PLAYER)
 Player::~Player() {}
 
 bool Player::Awake() {
-	position = Vector2D(1700, 1022);
+	position = Vector2D(3129, 945);
 	return true;
 }
 
